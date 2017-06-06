2052a5ff3e62c731e56b2b994d6a00cbcb29009f
opus1269 <igreilly1269@gmail.com>
2017-06-05 20:14:41 -0500

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2016-2017, Michael A. Updike All rights reserved.
+ * Licensed under Apache 2.0
+ * https://opensource.org/licenses/Apache-2.0
+ * https://github.com/opus1269/setting-elements/blob/master/LICENSE.md
+ */
+
+module.exports = {
+  'extends': [
+    'eslint:recommended',
+    'google',
+    'plugin:promise/recommended',
+  ],
+
+  'env': {
+    'browser': true,
+    'es6': true,
+  },
+
+  'plugins': [
+    'html',
+    'promise',
+  ],
+
+  'globals': {
+    'app': true,
+    'Polymer': true,
+    'Chrome': true,
+    'chrome': true,
+    'require': true,
+    'ChromePromise': true,
+    'ExceptionHandler': true,
+    'Headers': true,
+    'ga': true,
+  },
+
+  'rules': {
+    'linebreak-style': ['off', 'windows'],
+    'max-len': ['warn', 80],
+    'eqeqeq': ['error', 'always'],
+    'no-var': 'warn',
+    'no-console': ['warn', {'allow': ['error']}],
+    'no-unused-vars': 'warn',
+    'comma-dangle': ['warn', 'always-multiline'],
+    'no-trailing-spaces': 'off',
+    'padded-blocks': 'off',
+    'require-jsdoc': 'warn',
+    'new-cap': ['error', {'capIsNewExceptions': ['Polymer']}],
+    'quotes': ['error', 'single'],
+    'quote-props': ['error', 'consistent'],
+    'prefer-rest-params': 'off',
+    'valid-jsdoc': [
+      'error', {
+        'requireReturn': false,
+        'prefer': {
+          'return': 'returns',
+        },
+        'preferType': {
+          'Boolean': 'boolean',
+          'Number': 'number',
+          'object': 'Object',
+          'String': 'string',
+          'Integer': 'int',
+        },
+      }],
+  },
+
+};
---
+++ README.md
@@ -0,0 +1,4 @@
+## Material design settings elements based on Polymer 1.0
+
+[![bitHound Overall Score](https://www.bithound.io/github/opus1269/setting-elements/badges/score.svg)](https://www.bithound.io/github/opus1269/setting-elements)
+[![bitHound Code](https://www.bithound.io/github/opus1269/setting-elements/badges/code.svg)](https://www.bithound.io/github/opus1269/setting-elements)
